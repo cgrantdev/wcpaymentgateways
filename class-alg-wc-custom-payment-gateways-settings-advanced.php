836a629276ecc1a3de48881ffb3661d45dd04ddf
cgrantdev <grantcolin07@gmail.com>
2024-02-07 17:20:01 -0500

    Update class-alg-wc-custom-payment-gateways-settings-advanced.php

--- class-alg-wc-custom-payment-gateways-settings-advanced.php
+++ class-alg-wc-custom-payment-gateways-settings-advanced.php
@@ -1,47 +1,21 @@
 <?php
-/**
- * Custom Payment Gateways for WooCommerce - Advanced Section Settings
- *
- * @version 1.4.0
- * @since   1.4.0
- * @author  Imaginate Solutions
- * @package cpgw
- */
-
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
 if ( ! class_exists( 'Alg_WC_Custom_Payment_Gateways_Settings_Advanced' ) ) :
-
-	/**
-	 * Advanced Settings class.
-	 */
 	class Alg_WC_Custom_Payment_Gateways_Settings_Advanced extends Alg_WC_Custom_Payment_Gateways_Settings_Section {
 
 		public $id = '';
 
 		public $desc = '';
 
-		/**
-		 * Constructor.
-		 *
-		 * @version 1.4.0
-		 * @since   1.4.0
-		 */
 		public function __construct() {
 			$this->id   = 'advanced';
 			$this->desc = __( 'Advanced', 'custom-payment-gateways-woocommerce' );
 			parent::__construct();
 		}
 
-		/**
-		 * Get settings.
-		 *
-		 * @return array Settings Array.
-		 * @version 1.4.0
-		 * @since   1.4.0
-		 */
 		public function get_settings() {
 			$settings = array(
 				array(
