f46e4306c2e50a9ed118aa9daaa015f214461c05
cgrantdev <grantcolin07@gmail.com>
2024-02-07 17:21:59 -0500

    Update class-alg-wc-settings-custom-payment-gateways.php

--- class-alg-wc-settings-custom-payment-gateways.php
+++ class-alg-wc-settings-custom-payment-gateways.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * Custom Payment Gateways for WooCommerce - Settings
- *
- * @version 1.6.2
- * @since   1.0.0
- * @author  Imaginate Solutions
- * @package cpgw
- */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
@@ -14,17 +6,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 if ( ! class_exists( 'Alg_WC_Settings_Custom_Payment_Gateways' ) ) :
 
-	/**
-	 * Settings page class.
-	 */
 	class Alg_WC_Settings_Custom_Payment_Gateways extends WC_Settings_Page {
 
-		/**
-		 * Constructor.
-		 *
-		 * @version 1.6.2
-		 * @since   1.0.0
-		 */
 		public function __construct() {
 			$this->id    = 'alg_wc_custom_payment_gateways';
 			$this->label = __( 'Custom Payment Gateways', 'custom-payment-gateways-woocommerce' );
@@ -38,26 +21,10 @@ if ( ! class_exists( 'Alg_WC_Settings_Custom_Payment_Gateways' ) ) :
 			require_once 'class-alg-wc-custom-payment-gateways-settings-advanced.php';
 		}
 
-		/**
-		 * Maybe unsanitize option.
-		 *
-		 * @param mixed $value Value to pass.
-		 * @param mixed $option Option.
-		 * @param mixed $raw_value Raw Value.
-		 * @version 1.4.0
-		 * @since   1.4.0
-		 * @todo    [dev] find better solution
-		 */
 		public function maybe_unsanitize_option( $value, $option, $raw_value ) {
 			return ( ! empty( $option['alg_wc_cpg_raw'] ) ? $raw_value : $value );
 		}
 
-		/**
-		 * Get settings.
-		 *
-		 * @version 1.2.1
-		 * @since   1.0.0
-		 */
 		public function get_settings() {
 			global $current_section;
 			return array_merge(
@@ -83,12 +50,6 @@ if ( ! class_exists( 'Alg_WC_Settings_Custom_Payment_Gateways' ) ) :
 			);
 		}
 
-		/**
-		 * Maybe reset settings.
-		 *
-		 * @version 1.2.0
-		 * @since   1.0.0
-		 */
 		public function maybe_reset_settings() {
 			global $current_section;
 			if ( 'yes' === get_option( $this->id . '_' . $current_section . '_reset', 'no' ) ) {
@@ -102,23 +63,11 @@ if ( ! class_exists( 'Alg_WC_Settings_Custom_Payment_Gateways' ) ) :
 			}
 		}
 
-		/**
-		 * Admin notice settings reset.
-		 *
-		 * @version 1.2.1
-		 * @since   1.2.0
-		 */
 		public function admin_notice_settings_reset() {
 			echo '<div class="notice notice-warning is-dismissible"><p><strong>' .
 			esc_attr__( 'Your settings have been reset.', 'custom-payment-gateways-woocommerce' ) . '</strong></p></div>';
 		}
 
-		/**
-		 * Save.
-		 *
-		 * @version 1.0.0
-		 * @since   1.0.0
-		 */
 		public function save() {
 			parent::save();
 			$this->maybe_reset_settings();
